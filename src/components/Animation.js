e14f1e4eeac80b5ec9d2a8bddb2dfd6b390393d5
Jad Mintun <jadmintun@gmail.com>
2020-12-17 15:20:24 -0500

    add gsap

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -3,10 +3,17 @@ import React, { Suspense } from 'react'
 import { Canvas, useLoader, useFrame } from 'react-three-fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 import { OrbitControls, StandardEffects } from 'drei'
-
+import gsap from 'gsap'
 
 function TheModel() {
     const gltf = useLoader(GLTFLoader, '/schnitzel-01.glb')
+    const children = gltf.scene.children
+    for (let i = 0; i < children.length; i++) {
+        if (children[i].name === "Schnitzel") {
+            gsap.from(children[i].position, { duration: 1.5, x: -10, ease: 'elastic.inOut(1, 1)' })
+        }
+    }
+
     return <primitive object={gltf.scene} position={[0, 0, 0]} />
 }
 
@@ -37,8 +44,8 @@ function Animation() {
         <Canvas>
             <ambientLight />
             <pointLight position={[10, 10, 10]} />
-            <Box position={[-1.2, 0, 0]} />
-            <Box position={[1.2, 0, 0]} />
+            {/* <Box position={[-1.2, 0, 0]} /> */}
+            {/* <Box position={[1.2, 0, 0]} /> */}
             <Suspense fallback={null}>
                 <TheModel />
             </Suspense>

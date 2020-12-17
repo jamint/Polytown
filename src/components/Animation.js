f2b51c15ed52127da5cf427d37357c08ca20c412
Jad Mintun <jadmintun@gmail.com>
2020-12-17 14:41:54 -0500

    add react three fiber

--- src/components/Animation.js
+++ src/components/Animation.js
@@ -26,6 +26,8 @@ function Box(props) {
 function Animation() {
     return (
         <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
             <Box position={[-1.2, 0, 0]} />
             <Box position={[1.2, 0, 0]} />
         </Canvas>

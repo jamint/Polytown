4b89dfad951ac3d3b5a5efe2a78fb5cce1455d23
Jad Mintun <jadmintun@gmail.com>
2020-12-17 14:36:01 -0500

    update template

---
+++ src/components/Animation.js
@@ -0,0 +1,35 @@
+import { useRef, useState } from 'react'
+import { Canvas, useFrame } from 'react-three-fiber'
+
+function Box(props) {
+    const mesh = useRef()
+    const [hovered, setHover] = useState(false)
+    const [active, setActive] = useState(false)
+    useFrame(() => {
+        mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+    })
+
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+            onClick={(event) => setActive(!active)}
+            onPointerOver={(event) => setHover(true)}
+            onPointerOut={(event) => setHover(false)}>
+            <boxBufferGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    )
+}
+
+function Animation() {
+    return (
+        <Canvas>
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
+    );
+}
+
+export default Animation;

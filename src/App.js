d1f71db129be85ea06ab5a6c10cc2ae0f4dfaf02
Jad Mintun <jadmintun@gmail.com>
2020-12-18 16:42:12 -0500

    add garsone

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Header from './components/Header'
 import Animation from './components/Animation'
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Animation />
-    </div>
+    <RecoilRoot>
+      <div className="App">
+        <Header />
+        <Animation />
+      </div>
+    </RecoilRoot>
   );
 }
 
--- src/components/Animation.js
+++ src/components/Animation.js
@@ -2,52 +2,52 @@ import { useRef, useState } from 'react'
 import React, { Suspense } from 'react'
 import { Canvas, useLoader, useFrame } from 'react-three-fiber'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
-import { OrbitControls, StandardEffects } from 'drei'
+import { OrbitControls } from 'drei'
 import gsap from 'gsap'
 
-function TheModel() {
-    const gltf = useLoader(GLTFLoader, '/schnitzel-01.glb')
+function Schnitzel() {
+    const gltf = useLoader(GLTFLoader, '/garsone-01.glb')
     const children = gltf.scene.children
     for (let i = 0; i < children.length; i++) {
-        if (children[i].name === "Schnitzel") {
-            gsap.from(children[i].position, { duration: 1.5, x: -10, ease: 'elastic.inOut(1, 1)' })
-        }
+        // if (children[i].name === "Schnitzel") {
+        //     gsap.from(children[i].position, { duration: 1.5, x: -10, ease: 'elastic.inOut(1, 1)' })
+        // }
     }
 
     return <primitive object={gltf.scene} position={[0, 0, 0]} />
 }
 
-function Box(props) {
-    const mesh = useRef()
-    const [hovered, setHover] = useState(false)
-    const [active, setActive] = useState(false)
-    useFrame(() => {
-        mesh.current.rotation.x = mesh.current.rotation.y += 0.01
-    })
+// function Box(props) {
+//     const mesh = useRef()
+//     const [hovered, setHover] = useState(false)
+//     const [active, setActive] = useState(false)
+//     useFrame(() => {
+//         mesh.current.rotation.x = mesh.current.rotation.y += 0.01
+//     })
 
-    return (
-        <mesh
-            {...props}
-            ref={mesh}
-            scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
-            <boxBufferGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
-    )
-}
+//     return (
+//         <mesh
+//             {...props}
+//             ref={mesh}
+//             scale={active ? [1.5, 1.5, 1.5] : [1, 1, 1]}
+//             onClick={(event) => setActive(!active)}
+//             onPointerOver={(event) => setHover(true)}
+//             onPointerOut={(event) => setHover(false)}>
+//             <boxBufferGeometry args={[1, 1, 1]} />
+//             <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+//         </mesh>
+//     )
+// }
 
 function Animation() {
     return (
         <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]} />
+            <ambientLight intensity={0.3} />
+            <pointLight position={[10, 10, 10]} intensity={0.5} />
             {/* <Box position={[-1.2, 0, 0]} /> */}
             {/* <Box position={[1.2, 0, 0]} /> */}
             <Suspense fallback={null}>
-                <TheModel />
+                <Schnitzel />
             </Suspense>
             <OrbitControls />
         </Canvas>
